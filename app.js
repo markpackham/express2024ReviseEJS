64d1d16365c45ed73b9e17d9281f8d5d4c516e95
mark <markpackham1@gmail.com>
2024-05-16 18:27:34 +0100

    Demo ejs loop
    https://youtu.be/CnH3kAXSrmU?si=mmHQEAfOpTEvGp0z&t=6263

--- app.js
+++ app.js
@@ -6,4 +6,14 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.get("/", (req, res, next) => {
+  // Render is available to us thanks to ejs
+  res.render("index", {
+    title: "Welcome",
+    message: "This is rendered via EJS",
+    people: ["John", "Jane", "Jim", "Bob"],
+  });
+});
+
+// http://localhost:8000/
 app.listen(8000, () => console.log("Server started"));
